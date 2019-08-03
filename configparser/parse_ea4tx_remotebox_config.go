00868ba2155e8d13e91261829221d87797b487ee
ea4tx <ea4tx@ea4tx.com>
2019-08-03 22:57:10 +0200

    implmented additional remotebox versions and alternative tcp connection

--- configparser/parse_ea4tx_remotebox_config.go
+++ configparser/parse_ea4tx_remotebox_config.go
@@ -24,10 +24,19 @@ func GetEA4TXRemoteboxConfig(switchName string) ([]func(*rb.Remotebox), error) {
 		return nil, fmt.Errorf("missing portname parameter for switch %s", switchName)
 	}
 
+	if !viper.IsSet(fmt.Sprintf("%s.ipaddress", switchName)) {
+		return nil, fmt.Errorf("missing ipaddress parameter for switch %s", switchName)
+	}
+
 	name := rb.Name(viper.GetString(fmt.Sprintf("%s.name", switchName)))
 	index := rb.Index(viper.GetInt(fmt.Sprintf("%s.index", switchName)))
 	portname := rb.Portname(viper.GetString(fmt.Sprintf("%s.portname", switchName)))
+	// new options for TCP connection
+	rb_ipaddress := rb.Rb_IpAddress(viper.GetString(fmt.Sprintf("%s.ipaddress", switchName)))
+	rb_ipport := rb.Rb_IpPort(viper.GetInt(fmt.Sprintf("%s.ipport", switchName)))
+	rb_Connection := rb.Rb_Connection(viper.GetInt(fmt.Sprintf("%s.Connection", switchName)))
 
-	opts := []func(*rb.Remotebox){name, index, portname}
+	opts := []func(*rb.Remotebox){name, index, portname, rb_ipaddress, rb_ipport,rb_Connection}
+
 	return opts, nil
 }
