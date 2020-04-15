7e28a6e3ce47ab436b860805125f0c2bc3c2eaff
Tobias Wellnitz, DH1TW <tobias@dh1tw.de>
2020-04-15 11:32:37 +0200

    removed service init which overwrites registering interval (#5)

--- cmd/server_nats.go
+++ cmd/server_nats.go
@@ -199,9 +199,6 @@ func natsServer(cmd *cobra.Command, args []string) {
 		micro.Server(svr),
 	)
 
-	// initialize our service
-	ss.Init()
-
 	// before we annouce this service, we have to ensure that no other
 	// service with the same name exists. Therefore we query the
 	// registry for all other existing services.
